0291ceb63891901d288816357822d72d461d3919
Sxlis Admin <admin@sxlis.com>
2017-06-08 18:29:22 -0500

    Started on Dynamic Archive
    arch.py will eventually be the archive page, and will allow users to
    view archives of Solis's clothing

--- cgi-bin/arch.py
+++ cgi-bin/arch.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
 
-import cgi
+from config import page, mysql
 
-form = cgi.FieldStorage()
-print("Content-Type: text/html\n\n")
-print("""
-<!DOCTYPE html>
-<html>
-""")
-print("<h1>Test:", form["test"].value, "</h1>")
-print("</html>")
+print("Content-Type: text/html")
+
+########################
+#    Database Stuff    #
+########################
+from config import mysql
+import MySQLdb
+
+try:
+    cnx = MySQLdb.connect(mysql["host"], mysql["user"], mysql["pass"], mysql["database"])
+    
+except MySQLdb.error as err:
+
+    print(err)
+
+else:
+
+    cursor = cnx.cursor()
+    sql = "SELECT * f" #TODO: Get all items from the Items table
+    
+    try:
+
+        cursor.execute(sql)
+
+    except _mysql_exceptions.MySQLError as err:
+        print("<h1>There was an Error:</h1><br>")
+        print("<h2>")
+        print(err)
+        print("<br>")
+        print("</h2></body></html>")
+
+    else:
+        print()
+
+    cnx.close()
+
+print(page.format("test", "test", "test"))
---
+++ cgi-bin/config.py
@@ -0,0 +1,52 @@
+# MySQL Credentials
+mysql = {
+    "user" : "evosity_storedev",
+    "pass" : "x{_[ym?{fugGinXDqtZ7p~:Pu",
+    "host" : "127.0.0.1",
+    "database" : "evosity_storedev"
+
+}
+
+page = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>SOLIS 2018 S/S Archive</title>
+<link rel="stylesheet" type="text/css" href="/style/arch.css">
+</head>
+
+<body>
+
+<div class="title">
+<table>
+<td>{0}</td>
+</table>
+</div>
+<div class="item">
+<a href="PLACEHOLDER">
+<img src="/img/uploads/{1}";>
+</a>
+</div>
+<div class="description">
+<table>
+{2}
+</div>
+
+<div class="bogo">
+<div>
+<div>
+<a href="PLACEHOLDER" class="left">
+<img src="/img/left.png">
+</a>
+<a href="http://sxlis.com/" class="logo">
+<img src="/img/boxlogo.png">
+</a>
+<a href="PLACEHOLDER" class="right">
+<img src="/img/right.png">
+</a>
+</div>
+</div>
+</div>
+
+</body>
+"""
