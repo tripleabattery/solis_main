032d91052ca8e604f6a5c81f2ea7ace82d3b70c7
Sxlis Admin <admin@sxlis.com>
2017-06-17 01:00:08 -0500

    Items now sorted by ID
    Items used to be sorted by their location in the SQL table.
    
    Now, however, they are sorted by their ID number.

--- cgi-bin/arch.py
+++ cgi-bin/arch.py
@@ -28,7 +28,7 @@ else:
 
     cursor = cnx.cursor()
     sql = "SELECT * FROM `Items` WHERE `URL` = '{0}' " # Get items from table `Items`
-    sql2 = "SELECT `URL` FROM `Items`" # Get URL endings
+    sql2 = "SELECT * FROM `Items`" # Get URL endings
 
     try:
 
@@ -36,28 +36,29 @@ else:
         current = cursor.fetchone()
 
         try:
-            currentid = current[4]
+            currentid = current[0]
         except:
             print("TODO: Make this a 404 page :^)")
 
         cursor.execute(sql2)
         results = cursor.fetchall()
+        sresults = sorted(results)
 
-        for x in results:    # loop gets current item's index within the results list then sets currentidindex to this number
+        for x in sresults:    # loop gets current item's index within the results list then sets currentidindex to this number
             if currentid == x[0]:
-                print
-                currentidindex = results.index(x)
+                currentidindex = sresults.index(x)
                 break
 
-        try:
-            nextitem = results[currentidindex+1]
-        except:
-            nextitem = results[0]    # If there is no nextitem, set nextitem to the first item
 
         try:
-            previtem = results[currentidindex-1]
+            nextitem = sresults[currentidindex+1]
         except:
-            previtem = results[-1]    # If there is no previtem, set previtem to the last item
+            nextitem = sresults[0]    # If there is no nextitem, set nextitem to the first item
+
+        try:
+            previtem = sresults[currentidindex-1]
+        except:
+            previtem = sresults[-1]    # If there is no previtem, set previtem to the last item
 
     except _mysql_exceptions.MySQLError as err:
         print("<h1>There was an Error:</h1><br>")
@@ -66,6 +67,6 @@ else:
         print("</h2></body></html>")
 
     else:
-        print(page.format(title=current[1], desc=current[2], image=current[3], idprev=previtem[0], idnext=nextitem[0]))
+        print(page.format(title=current[1], desc=current[2], image=current[3], idprev=previtem[4], idnext=nextitem[4]))
 
     cnx.close()
